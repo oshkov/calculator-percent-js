1ca8977717d4e79e4cd040497ecfe7858fd519c2
Alexander Oshkov <sasha.oshkov03@gmail.com>
2023-05-19 23:12:50 +0300

    v2.0 percent calculator done

--- script.js
+++ script.js
@@ -6,16 +6,26 @@ function calculate( {a, b} ) {
     return result;
 }
 
+function deleteResult(num) {
+ 
+    // Выбираются блок и кнопка, чтобы удалить их
+    let outputBlock = document.getElementsByClassName("js-output-block " + num)[0]
+    let outputFlexBtn = document.getElementsByClassName("js-output-flex-button " + num)[0]
 
-// Работа с версткой
+    outputBlock.style = " padding: 0 0 0 700px" // Плавно изменяется паддинг
+    outputFlexBtn.remove() // Сразу удаляется кнопка, после нажатия
 
-const inputANode = document.querySelector(".js-input-a")
-const inputBNode = document.querySelector(".js-input-b")
-const btnResultNode = document.querySelector(".js-btn-result")
+    // Таймер для удаления всего блока (Сработает через 0.5 сек)
+    setTimeout(() => {
+        outputBlock.remove()
+    }, 500); 
+}
+
+
 let num = 0
-
-
-btnResultNode.addEventListener("click", function() {
+function result() {
+    const inputANode = document.querySelector(".js-input-a")
+    const inputBNode = document.querySelector(".js-input-b")
 
     // Поиск блока в HTML куда будет добавляться новый блок с ответами
     let Block = document.querySelector(".block")
@@ -40,27 +50,65 @@ btnResultNode.addEventListener("click", function() {
     num++
 
     // Создание блока с результатом
+    let html0 = document.createElement('div')
+    html0.className = "js-output-flex " + num
+
     let html1 = document.createElement('h1')
     html1.className = "js-output-result " + num
     html1.textContent = result
 
+    let html2 = document.createElement('p')
+    html2.className = "js-output-a-b " + num
+    html2.textContent = a + ' → ' + b
+
+    html0.append(html1)
+    html0.append(html2)
+
+    let html4 = document.createElement('div')
+    html4.className = "js-output-flex-button " + num
+
+    let htmlBtn = document.createElement('button')
+    htmlBtn.className = "js-delete-button " + num
+    htmlBtn.setAttribute("onclick","deleteResult("+num+")");
+    
+    htmlBtn.textContent = '×'
+
+    html4.append(htmlBtn)
+
     let html = document.createElement('div')
     html.className = "js-output-block " + num
 
-    html.append(html1)
+    html.append(html0)
+    html.append(html4)
+
 
     // Добавление блока с результатом в HTML
     Block.insertBefore(html, document.querySelectorAll(".js-output-block")[0])
 
     let outputBlock = document.querySelector(".js-output-block")
-    document.querySelectorAll(".js-output-block")[0].style= "max-height: "+ outputBlock.scrollHeight +"px" // Максимальная высота блока изменяется высоту блока
+    document.querySelectorAll(".js-output-block")[0].style= "max-height: "+ outputBlock.scrollHeight +"px; padding: 15px; overflow: visible" // Максимальная высота блока изменяется высоту блока
 
     // Проверка на наличие второго результата, при его наличии он становится прозрачнее
     let nextResult = document.querySelectorAll(".js-output-result")[1]
     if (nextResult != null) {
         nextResult.style= "color: rgba(0, 0, 0, 0.4)"
+        document.querySelectorAll(".js-output-a-b")[1].style= "color: rgba(0, 0, 0, 0.2)"
     } else {
         return
     }
+}
 
-});
+
+
+
+// Строчки ограничивающие ввод в input до 18 символов
+
+let inputA = document.querySelector('.js-input-a');
+inputA.oninput = function(){
+  this.value = this.value.substr(0, 18);
+}
+
+let inputB = document.querySelector('.js-input-b');
+inputB.oninput = function(){
+  this.value = this.value.substr(0, 18);
+}
